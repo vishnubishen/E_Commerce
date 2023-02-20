b8f0efabd0eb1cdc16428c1455465bafce5f2ec7
Vishnu_Balasubramaniyan <vishnu.balasubramaniyan@axeno.co>
2023-02-20 19:44:03 +0530

    Ecommerce starter kit

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">Welcome to Autumn Fashion 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> An Ecommerce Website With React JS And Tailwind CSS
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm start
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **Vishnu**
+
+- Github: [@vishnu97](https://github.com/vishnubishen)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  const { products } = useContext(ProductContext);
+
+  const filterredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  return (
+    <div>
+      <Hero />
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0 ">
+            {filterredProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
